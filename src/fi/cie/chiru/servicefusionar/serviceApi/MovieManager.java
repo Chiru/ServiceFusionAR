a2e8a22ebc1bffc81aa9a4cd93bd624a6e53e6d3
antti <antti@antti-HP-EliteBook-8740w.(none)>
2013-02-07 10:06:23 +0200

    MovieManager object with stub functions added. This class is responsible for handling the logic of buying movie tickets.

---
+++ src/fi/cie/chiru/servicefusionar/serviceApi/MovieManager.java
@@ -0,0 +1,36 @@
+package fi.cie.chiru.servicefusionar.serviceApi;
+
+import util.Log;
+
+
+public class MovieManager
+{
+	private static final String LOG_TAG = "MovieManager";
+	TextPopUp movieInformation;
+	
+	public MovieManager()
+	{
+//		movieInformation = new TextPopUp(); 	
+	}
+	
+    public void showMovieInfo()
+    {
+    	Log.d(LOG_TAG, "---------------------------showMovieInfo()");
+    }
+    
+    public void seatSelection()
+    {
+    	
+    }
+    
+    public void login()
+    {
+    	Log.d(LOG_TAG, "---------------------------login()");
+    }
+    
+    public void payment()
+    {
+    	
+    }
+
+}
