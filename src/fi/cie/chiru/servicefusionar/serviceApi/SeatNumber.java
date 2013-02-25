3be6a9b9583b7e3fa3e371a8728664c3fd4e77ea
antti <antti@antti-HP-EliteBook-8740w.(none)>
2013-02-25 16:09:32 +0200

    This class is created to help the seat selection process and the communication between the Auditorium and the moviePayment classes.

---
+++ src/fi/cie/chiru/servicefusionar/serviceApi/SeatNumber.java
@@ -0,0 +1,38 @@
+package fi.cie.chiru.servicefusionar.serviceApi;
+
+public class SeatNumber {
+	
+	private int row;
+	private int col;
+
+	public SeatNumber(int row, int col)
+	{
+	    this.row = row;
+	    this.col = col;
+	}
+	
+	public int getRow() 
+	{
+		return row;
+	}
+	public void setRow(int row) 
+	{
+		this.row = row;
+	}
+	public int getCol() 
+	{
+		return col;
+	}
+	public void setCol(int col) 
+	{
+		this.col = col;
+	}
+
+	public boolean areEquals(SeatNumber s)
+	{
+		if(s.getCol() == this.getCol() && s.getRow() == this.getRow())
+			return true;
+		else
+			return false;
+	}
+}
