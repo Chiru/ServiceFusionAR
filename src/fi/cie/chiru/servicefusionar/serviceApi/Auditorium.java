17ee1c46d0fc841bf3c5d074408e9935a70bbe88
antti <antti@antti-HP-EliteBook-8740w.(none)>
2013-02-25 16:07:35 +0200

    The seat selection finalized. The user selected seats will be forwarded to the moviePayment class.

--- src/fi/cie/chiru/servicefusionar/serviceApi/Auditorium.java
+++ src/fi/cie/chiru/servicefusionar/serviceApi/Auditorium.java
@@ -1,6 +1,12 @@
 package fi.cie.chiru.servicefusionar.serviceApi;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
+import commands.Command;
+import commands.system.CommandDeviceVibrate;
+
 import util.IO;
 import util.Log;
 import util.Vec;
@@ -14,6 +20,7 @@ import android.os.Looper;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.GridLayout;
 import android.widget.GridLayout.Spec;
 import android.widget.ImageView;
@@ -25,9 +32,11 @@ public class Auditorium
 	private MeshComponent plaza;
 	private ServiceManager serviceManager;
 	private int plaza1Margins [][][];
+	private List<SeatNumber> selectedSeats;
 	private Bitmap green_seat;
 	private Bitmap wheelchair_seat;
 	private Bitmap yellow_seat;
+	private Command vibratefeedback;
 	
 	public Auditorium(ServiceManager serviceManager)
 	{
@@ -35,6 +44,8 @@ public class Auditorium
 		green_seat = IO.loadBitmapFromId(serviceManager.getSetup().myTargetActivity, R.drawable.green_seat);
 		wheelchair_seat = IO.loadBitmapFromId(serviceManager.getSetup().myTargetActivity, R.drawable.wheelchair);
 		yellow_seat = IO.loadBitmapFromId(serviceManager.getSetup().myTargetActivity, R.drawable.yellow_seat);
+		vibratefeedback = new CommandDeviceVibrate(serviceManager.getSetup().myTargetActivity, 30);
+		selectedSeats = new ArrayList<SeatNumber>();
 	}
 	
 	public void createAuditoriumScreen(final String plazaName)
@@ -70,6 +81,7 @@ public class Auditorium
 		initPlaza1Margins();
 		GridLayout gl = (GridLayout)l.inflate(R.layout.movie_seatselection2, null);		
 		setPlaza1Seats(gl);
+		connectButtons(gl);
 		View sv = (View)gl;
 		
 		return sv;
@@ -82,9 +94,15 @@ public class Auditorium
 		GridLayout.LayoutParams p;
 		ImageView seat;
 		gl.setAlignmentMode(GridLayout.ALIGN_BOUNDS);
+		
+		int seatCol;
+		int seatRow = gl.getRowCount();
 
 		for(int j=0; j<gl.getRowCount()-1; j++)
 		{
+			seatRow--;
+			seatCol = 0;
+			
 		    for(int i=0; i<gl.getColumnCount()-1; i++)
 		    {
 		    	
@@ -101,6 +119,8 @@ public class Auditorium
 			    rowSpec = GridLayout.spec(j, 1);
                 p = new GridLayout.LayoutParams(rowSpec, colSpec);
                 
+                seatCol++;
+                
         		if(plaza1Margins[i][j] != null)
 			        p.setMargins(plaza1Margins[i][j][0], plaza1Margins[i][j][1], plaza1Margins[i][j][2], plaza1Margins[i][j][3]);
         		
@@ -111,16 +131,17 @@ public class Auditorium
 			    if(((i>4 && i<8) || (i>25 && i<29)) && j==14)
 			    {
 			        seat.setImageBitmap(wheelchair_seat);
-			        seat.setTag("wheelchair");
+			        seat.setTag(new SeatTag(seatRow, seatCol, "wheelchair"));
 			    }
 			    else
 			    {
 			        seat.setImageBitmap(green_seat);
-			        seat.setTag("green_seat");
+			        seat.setTag(new SeatTag(seatRow, seatCol,"green_seat"));
 			    }
-			    
+
 			    seat.setOnClickListener(new View.OnClickListener() 
 			    {
+			    	@Override
                     public void onClick(View v) 
                     {
                         changeSeat(v);
@@ -129,34 +150,71 @@ public class Auditorium
 			    
 			    gl.addView(seat, p);
 			}
+		    
 		}
 	}
 	
 	private void changeSeat(View v)
 	{
     	ImageView iv = (ImageView)v;
-    	String tag = (String)iv.getTag();
-    	
-    	if(tag.compareTo("green_seat")==0)
+    	SeatTag tag = (SeatTag)iv.getTag();
+
+    	if(tag.getType().compareTo("green_seat")==0)
     	{
     	    iv.setImageBitmap(yellow_seat);
-    	    iv.setTag("yellow_seat");
+    	    tag.setType("yellow_seat");
+    	    selectedSeats.add(tag.getSeatNum());
     	}
-    	else if(tag.compareTo("wheelchair")==0)
+    	else if(tag.getType().compareTo("wheelchair")==0)
     	{
     		iv.setImageBitmap(yellow_seat);
-     	    iv.setTag("yellow_seat_wheelchair");	
+    		tag.setType("yellow_seat_wheelchair");
+    		selectedSeats.add(tag.getSeatNum());
     	}
-    	else if(tag.compareTo("yellow_seat")==0)
+    	else if(tag.getType().compareTo("yellow_seat")==0)
     	{
     		iv.setImageBitmap(green_seat);
-     	    iv.setTag("green_seat");	
+    		tag.setType("green_seat");
+    		selectedSeats.remove(tag.getSeatNum());
     	}
-    	else if(tag.compareTo("yellow_seat_wheelchair")==0)
+    	else if(tag.getType().compareTo("yellow_seat_wheelchair")==0)
     	{
     		iv.setImageBitmap(wheelchair_seat);
-     	    iv.setTag("wheelchair");	
+    		tag.setType("wheelchair");
+    		selectedSeats.remove(tag.getSeatNum());
     	}
+	}
+	
+	private void connectButtons(GridLayout gl)
+	{
+		Button okb = (Button)gl.findViewById(R.id.seat_ok);
+		okb.setOnClickListener(new View.OnClickListener()
+		{
+	        
+			@Override
+	        public void onClick(View v) 
+	        {
+				vibratefeedback.execute();
+				RelativeLayout root = (RelativeLayout) serviceManager.getSetup().getGuiSetup().getMainContainerView();
+				root.removeView(plazaView);
+				serviceManager.getMovieManager().payment(selectedSeats);
+	        }
+	    });
+		
+		Button cancelb = (Button)gl.findViewById(R.id.seat_peruuta);
+		cancelb.setOnClickListener(new View.OnClickListener()
+		{
+	        
+			@Override
+	        public void onClick(View v) 
+	        {
+				vibratefeedback.execute();
+				RelativeLayout root = (RelativeLayout) serviceManager.getSetup().getGuiSetup().getMainContainerView();
+				root.removeView(plazaView);
+				serviceManager.setVisibilityToAllApplications(true);
+				serviceManager.getMovieManager().getInfoBubble().visible();
+	        }
+	    });
 	}
 	
 	private void initPlaza1Margins()
@@ -197,6 +255,37 @@ public class Auditorium
 	    plaza1Margins[32][0] = new int[] {3,74,3,0};
 	    plaza1Margins[33][0] = new int[] {3,74,3,0};
 	    plaza1Margins[33][0] = new int[] {3,74,120,0};
+	}
+	
+	private class SeatTag {
+		
+        private SeatNumber seatNum;
+        private String type;
+		
+		public SeatTag(int row, int col, String type)
+		{
+		    this.seatNum = new SeatNumber(row, col);
+		    this.type = type;
+		}
+
+		public String getType() 
+		{
+			return type;
+		}
+
+		public void setType(String type) 
+		{
+			this.type = type;
+		}
+
+		public SeatNumber getSeatNum() {
+			return seatNum;
+		}
+
+		public void setSeatNum(SeatNumber seatNum) {
+			this.seatNum = seatNum;
+		}
+		
 	}
 }
 
