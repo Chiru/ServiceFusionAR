dfc35becb8b901d860d5ef3af4d16f84bdb6da61
antti <antti@antti-HP-EliteBook-8740w.(none)>
2013-03-08 17:13:17 +0200

    Don't try to remove tickets when they don't exist.

--- src/fi/cie/chiru/servicefusionar/serviceApi/MovieManager.java
+++ src/fi/cie/chiru/servicefusionar/serviceApi/MovieManager.java
@@ -108,6 +108,9 @@ public class MovieManager
     
     public void removeTickets()
     {
+    	if(tickets == null)
+    		return;
+    	
     	for(int i=0; i<tickets.size(); i++)
     	{
     		tickets.get(i).removeTicket();
