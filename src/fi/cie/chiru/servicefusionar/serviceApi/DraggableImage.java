8a1a38ccaa98aa0703a465b09d64d17b492bd376
antti <antti@antti-HP-EliteBook-8740w.(none)>
2013-03-08 17:09:40 +0200

    Code structure changed. Fixes the bug, that when images were dragged the shadow image was never rendered in first try.

--- src/fi/cie/chiru/servicefusionar/serviceApi/DraggableImage.java
+++ src/fi/cie/chiru/servicefusionar/serviceApi/DraggableImage.java
@@ -1,6 +1,8 @@
 package fi.cie.chiru.servicefusionar.serviceApi;
 
 import android.content.ClipData;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.View;
 import android.view.View.DragShadowBuilder;
 import android.widget.ImageView;
@@ -14,28 +16,39 @@ public class DraggableImage extends CommandInUiThread
 	private String str;
 	private ServiceManager serviceManager;
 	
-	public DraggableImage(ServiceManager serviceManager, ImageView DraggedItem, String str)
+	public DraggableImage(ServiceManager serviceManager, ImageView draggedItem, String str)
 	{
-	    iv = DraggedItem;
+	    iv = draggedItem;
 	    this.str = str;
 	    this.serviceManager = serviceManager;
+	    setImageViewToLayout();
 	}
 	
 //	@Override
 	public void executeInUiThread()
 	{
-
-		RelativeLayout root = (RelativeLayout)serviceManager.getSetup().getGuiSetup().getMainContainerView();
-
-		root.removeView(iv);
-		root.addView(iv);
-		
-		//iv.setVisibility(View.GONE);
-		iv.setVisibility(View.INVISIBLE);
-	    
 		View.DragShadowBuilder shadow = new DragShadowBuilder(iv);
 		ClipData data = ClipData.newPlainText("DragData", str);
 		iv.startDrag(data, shadow, null, 0);
+	}
+	
+	private void setImageViewToLayout()
+	{
+		 Handler mHandler = new Handler(Looper.getMainLooper());
+
+		 mHandler.post(new Runnable() 
+		 {
+
+			 @Override
+			 public void run() 
+			 {
+				 RelativeLayout root = (RelativeLayout)serviceManager.getSetup().getGuiSetup().getMainContainerView();
+				 root.removeView(iv);
+				 root.addView(iv);
+				 iv.setVisibility(View.INVISIBLE);
+			 }
+
+		 });
 	}
 	
 }
