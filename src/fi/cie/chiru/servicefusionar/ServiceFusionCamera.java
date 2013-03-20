6ef03a5e4d895865b6b4f21749083e35e287d843
antti <antti@antti-HP-EliteBook-8740w.(none)>
2013-03-20 13:12:23 +0200

    The logic of updating the camera and attached meshes is changed. Now the camera gets the current angle via sensors and updates it's own rotation and the meshes positions and rotations when it's rendering function is called.

--- src/fi/cie/chiru/servicefusionar/ServiceFusionCamera.java
+++ src/fi/cie/chiru/servicefusionar/ServiceFusionCamera.java
@@ -4,13 +4,18 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Vector;
 
+import javax.microedition.khronos.opengles.GL10;
+
 import android.opengl.Matrix;
 
 import util.Log;
 import util.Vec;
 import worldData.Updateable;
+import fi.cie.chiru.servicefusionar.sensors.Sensors;
 import fi.cie.chiru.servicefusionar.serviceApi.ServiceApplication;
+import fi.cie.chiru.servicefusionar.serviceApi.ServiceManager;
 import gl.GLCamera;
+import gl.Renderable;
 import gl.scenegraph.MeshComponent;
 
 public class ServiceFusionCamera extends GLCamera
@@ -18,24 +23,13 @@ public class ServiceFusionCamera extends GLCamera
 	private List<MeshComponent> attachedMeshes;
 	private float prevAngle;
 	private float currentAngle;
-	private boolean angleChanged = false;
-	Vec up = new Vec(0.0f, 1.0f, 0.0f);
+	ServiceManager serviceManager = null;
 	
-	public ServiceFusionCamera(Vec initialCameraPosition)
+	public ServiceFusionCamera(ServiceManager serviceManager, Vec initialCameraPosition)
 	{
 		super(initialCameraPosition);
+		this.serviceManager = serviceManager;
 		attachedMeshes = new ArrayList<MeshComponent>();
-	}
-	
-	@Override
-	public boolean update(float timeDelta, Updateable parent) 
-	{
-//		if(angleChanged)
-//		{
-//			updateMeshPositions();
-//			angleChanged = false;
-//		}
-		return true;//super.update(timeDelta, parent);
 	}
 	
 	public void attachToCamera(MeshComponent mc)
@@ -47,44 +41,47 @@ public class ServiceFusionCamera extends GLCamera
 	{
 		attachedMeshes.remove(mc);
 	}
-	
-	public void setNewAngle(float angle)
+
+	@Override
+	public synchronized void render(GL10 gl, Renderable parent) 
 	{
 		prevAngle = currentAngle;
-		currentAngle = angle;
-		this.setRotation(0.0f, angle, 0.0f);
-		//angleChanged = true;
+		currentAngle = serviceManager.getSensors().getCurrentAngle();
+		this.setRotation(0.0f, currentAngle, 0.0f);
 		updateMeshPositions();
+		super.render(gl, parent);
 	}
 	
 	private void updateMeshPositions()
 	{
 		float angle = currentAngle - prevAngle;
 		
+		if(attachedMeshes.isEmpty())
+			return;
+		
 		if(angle == 0)
 			return;
 		
 		float rotAngleRad = (float)(angle*(Math.PI/180));
-		Vec direction = new Vec();
+		
+		float sin = (float)Math.sin(rotAngleRad);
+		float cos = (float)Math.cos(rotAngleRad);
+		float[] rotMat = {cos, 0, sin, 0, 0, 1, 0, 0, -sin, 0, cos, 0, 0,0,0,1};
 		
 		for(int i = 0; i<attachedMeshes.size(); i++)
 		{
 			MeshComponent meshComp = attachedMeshes.get(i);
 			Vec meshPos = meshComp.getPosition();
 			Vec meshRot = meshComp.getRotation();
-			Vec camRot = this.getRotation();
+			//Vec camRot = this.getRotation();
 			Vec camPos = this.getPosition();
 
-			float sin = (float)Math.sin(rotAngleRad);
-			float cos = (float)Math.cos(rotAngleRad);
-			float[] rotMat = {cos, 0, sin, 0, 0, 1, 0, 0, -sin, 0, cos, 0, 0,0,0,1};
 			float [] pos ={meshPos.x, meshPos.y, meshPos.z, 1.0f};
 			float [] res ={0.0f, 0.0f, 0.0f, 0.0f};
 			Matrix.multiplyMV(res, 0, rotMat, 0, pos, 0);
 			meshPos.setTo(camPos.x + res[0], camPos.y + res[1], camPos.z + res[2]);
 			
-			meshRot.setTo(meshRot.x, -camRot.y , meshRot.z);
-            
+			meshRot.setTo(meshRot.x, -currentAngle , meshRot.z);
 		}		
 	}
 }
