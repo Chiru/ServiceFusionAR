364ab57391a65ae5016661916ee795e5081d0901
antti <antti@antti-HP-EliteBook-8740w.(none)>
2013-03-25 16:11:48 +0200

    Camera can be set to UI mode which clears the camera rotation and freezes the video feed. The video feed is also freezed when the device is tilted.

--- src/fi/cie/chiru/servicefusionar/ServiceFusionCamera.java
+++ src/fi/cie/chiru/servicefusionar/ServiceFusionCamera.java
@@ -2,16 +2,11 @@ package fi.cie.chiru.servicefusionar;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Vector;
-
 import javax.microedition.khronos.opengles.GL10;
-
 import android.opengl.Matrix;
-
-import util.Log;
+import util.EfficientList;
 import util.Vec;
-import worldData.Updateable;
-import fi.cie.chiru.servicefusionar.sensors.Sensors;
+import worldData.RenderableEntity;
 import fi.cie.chiru.servicefusionar.serviceApi.ServiceApplication;
 import fi.cie.chiru.servicefusionar.serviceApi.ServiceManager;
 import gl.GLCamera;
@@ -23,6 +18,8 @@ public class ServiceFusionCamera extends GLCamera
 	private List<MeshComponent> attachedMeshes;
 	private float prevAngle;
 	private float currentAngle;
+	private boolean UImode = false;
+	private boolean deviceTilted = false;
 	ServiceManager serviceManager = null;
 	
 	public ServiceFusionCamera(ServiceManager serviceManager, Vec initialCameraPosition)
@@ -45,11 +42,39 @@ public class ServiceFusionCamera extends GLCamera
 	@Override
 	public synchronized void render(GL10 gl, Renderable parent) 
 	{
-		prevAngle = currentAngle;
-		currentAngle = serviceManager.getSensors().getCurrentAngle();
-		this.setRotation(0.0f, currentAngle, 0.0f);
-		updateMeshPositions();
-		super.render(gl, parent);
+		if(UImode == false)
+		{
+			if(!deviceTilted && serviceManager.getSensors().tilted())
+			{
+				serviceManager.getSetup().myCameraView.pause();
+				deviceTilted = true;
+			}
+			else if(deviceTilted && !serviceManager.getSensors().tilted())
+			{
+				serviceManager.getSetup().myCameraView.resumeCamera();
+				deviceTilted = false;
+			}
+			
+		    prevAngle = currentAngle;
+		    currentAngle = serviceManager.getSensors().getCurrentAngle();
+		    this.setRotation(0.0f, currentAngle, 0.0f);
+		    updateMeshPositions();
+		    faceMeshesToCamera();
+		    super.render(gl, parent);
+		}
+	}
+	
+	public void setUIMode(boolean mode)
+	{
+		if(mode == true)
+		{
+		    this.setRotation(0.0f, 0.0f, 0.0f);
+		    serviceManager.getSetup().myCameraView.pause();
+		}
+		else
+			serviceManager.getSetup().myCameraView.resumeCamera();
+		
+		UImode = mode;
 	}
 	
 	private void updateMeshPositions()
@@ -72,16 +97,35 @@ public class ServiceFusionCamera extends GLCamera
 		{
 			MeshComponent meshComp = attachedMeshes.get(i);
 			Vec meshPos = meshComp.getPosition();
-			Vec meshRot = meshComp.getRotation();
-			//Vec camRot = this.getRotation();
 			Vec camPos = this.getPosition();
 
 			float [] pos ={meshPos.x, meshPos.y, meshPos.z, 1.0f};
 			float [] res ={0.0f, 0.0f, 0.0f, 0.0f};
 			Matrix.multiplyMV(res, 0, rotMat, 0, pos, 0);
 			meshPos.setTo(camPos.x + res[0], camPos.y + res[1], camPos.z + res[2]);
-			
-			meshRot.setTo(meshRot.x, -currentAngle , meshRot.z);
 		}		
+	}
+	
+	private void faceMeshesToCamera()
+	{
+		EfficientList<RenderableEntity> entities = serviceManager.getSetup().world.getAllItems();
+		
+		for(int i = 0; i<entities.myLength; i++)
+		{
+			RenderableEntity e = entities.get(i);
+			if(e instanceof MeshComponent)
+			{
+				MeshComponent meshComp = (MeshComponent)e;
+				Vec meshRot = meshComp.getRotation();
+				meshRot.setTo(meshRot.x, -currentAngle , meshRot.z);
+			}
+			else if(e instanceof ServiceApplication)
+			{
+				ServiceApplication serviceApp = (ServiceApplication)e;
+				MeshComponent meshComp = serviceApp.getMesh();
+				Vec meshRot = meshComp.getRotation();
+				meshRot.setTo(meshRot.x, -currentAngle , meshRot.z);
+			}
+		}
 	}
 }
